aa451123a2bbdf76d904f608b00059865cc802aa
pawanchoubey578 <choubeypawan578@gmail.com>
2024-10-20 21:25:15 +0530

    Create script.js

---
+++ public/script.js
@@ -0,0 +1,4 @@
+document.getElementById('threshold-form').addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const city = document.getElementById('city').value;
+    const threshold = document.getElementById('threshold
